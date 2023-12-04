1c3b98c229c5e6919d12ffd091f00f9b90e8458b
vukamidzic <vuk.amidzic@gmail.com>
2023-12-04 13:20:45 +0100

    [Nim] Solved first part of day 2

---
+++ nim/day2.nim
@@ -0,0 +1,35 @@
+import strutils
+import sugar
+import sequtils
+import tables
+
+proc stripData(data: string): seq[string] = 
+    data.split(';')
+        .map(s => s.strip(true, false))
+
+proc part1(filePath: string): int = 
+    let lines = readFile(filePath).splitLines()
+    var 
+        sum = 0
+        gameNum = 1
+    for line in lines:
+        var data = line.split(':')
+        let rounds = stripData(data[1].strip(true, false))
+        var possible = true
+        for round in rounds:
+            let curr = round.split(',').map(s => s.strip(true, false))
+            for balls in curr:
+                let 
+                    info = balls.split(' ')
+                    color = info[1]
+                    n = parseInt(info[0])
+                case color:
+                    of "red": 
+                        if n > 12: possible = false
+                    of "blue":
+                        if n > 14: possible = false
+                    of "green": 
+                        if n > 13: possible = false
+        if possible: sum += gameNum
+        gameNum += 1 
+    return sum
