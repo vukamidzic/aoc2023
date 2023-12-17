6e61b81b20c0bf693a29e49573e92b6dc00ed9e6
vukamidzic <vuk.amidzic@gmail.com>
2023-12-17 21:12:00 +0100

    small changes

--- nim/day11.nim
+++ nim/day11.nim
@@ -36,34 +36,3 @@ proc part1(filePath: string): int =
 
     var positions = getPositions(galaxies)
     return shortestDists(positions)
-
-proc initEmptys(n: int): string =
-    result = ""
-    for i in 1..n: result.add('.')
-
-proc part2(filePath: string): int = 
-    var lines = readFile(filePath).splitLines()
-    var galaxies: seq[string] = @[] 
-    for line in lines:
-        galaxies.add(line)
-        if not line.contains('#'): 
-            for i in 1..9: galaxies.add(line)
-
-    var toEvade: seq[int] = @[]
-    for j in 0..len(galaxies[0])-1:
-        var vertLine = ""
-        for i in 0..len(galaxies)-1: vertLine.add(galaxies[i][j])
-        if (not vertLine.contains('#')) and (not toEvade.contains(j)):
-            for i in 0..len(galaxies)-1:
-                let tmp = galaxies[i].substr(j+1, len(galaxies[0]))
-                galaxies[i] = galaxies[i].substr(0,j)
-                galaxies[i].add(initEmptys(9))
-                galaxies[i].add(tmp)
-            for i in 1..8:
-                toEvade.add(j + i)
-    echo toEvade
-    print galaxies
-    var positions = getPositions(galaxies)
-    return shortestDists(positions)
-
-echo part2("test.txt")
