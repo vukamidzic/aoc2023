60b4712531ca3ce786e083cb8933abbdb72bdcd8
vukamidzic <vuk.amidzic@gmail.com>
2024-01-03 13:50:09 +0100

    [Nim] Solved second part of day 18

--- nim/day18.nim
+++ nim/day18.nim
@@ -3,6 +3,7 @@ import sequtils
 import algorithm
 import sugar
 import print
+import tables
 
 type Pos = (int, int)
 
@@ -47,4 +48,39 @@ proc part1(filepath: string): int =
                 index = index + 1
     result = area(positions)
 
-echo part1("input.txt")
+proc part2(filepath: string): int = 
+    let lines = readFile(filepath).splitLines()
+    let dirs = {
+        '0': "R",
+        '1': "D",
+        '2': "L",
+        '3': "U"
+    }.toTable 
+    var 
+        positions: seq[Pos] = @[(0,0)]
+        index = 0
+    for line in lines:
+        var info = line.split(" ")[2]
+        removePrefix(info, "(")
+        removeSuffix(info, ")")
+        let (steps, dir) = (parseHexInt(info[0..len(info)-2]), dirs[info[len(info)-1]])
+        case dir:
+            of "R":
+                let new_pos: Pos = (positions[index][0], positions[index][1] + steps)
+                positions.add(new_pos)
+                index = index + 1
+            of "L":
+                let new_pos: Pos = (positions[index][0], positions[index][1] - steps)
+                positions.add(new_pos)
+                index = index + 1
+            of "D":
+                let new_pos: Pos = (positions[index][0] - steps, positions[index][1])
+                positions.add(new_pos)
+                index = index + 1
+            of "U":
+                let new_pos: Pos = (positions[index][0] + steps, positions[index][1])
+                positions.add(new_pos)
+                index = index + 1
+    result = area(positions)
+
+echo part2("input.txt")
